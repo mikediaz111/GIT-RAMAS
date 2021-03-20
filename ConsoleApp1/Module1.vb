21392e3d459c33fad2f79ca4745a7f827d8fbe34
Miguel <miguel.diaz5@catolica.edu.sv>
2021-03-20 01:24:59 -0600

    1er commit

---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module module1
+
+    Sub main()
+        Dim nombres(4) As String
+
+        For i = 0 To nombres.Length - 1 Step 1
+
+            Console.WriteLine("ingrese nombre " & (If + 1))
+            nombres(i) = Console.ReadLine
+            'segunda prueba
+            'segunda prueba
+            'segunda prueba
+        Next
+        For i = 0 To nombres.Length - 1 Step 1
+
+            Console.WriteLine("ingrese nombre " & (If + 1))
+            nombres(i) = Console.ReadLine
+            'segunda prueba
+            'segunda prueba
+            'segunda prueba
+        Next
+        For i = 0 To nombres.Length - 1 Step 1
+            Console.WriteLine("nombre " & (If + 1) & " : " & nombres(i))
+
+
+        Next
+
+        For i = 0 To nombres.Length - 1 Step 1
+            Console.WriteLine("nombre " & (If + 1) & " : " & nombres(i))
+
+            '
+        Next
+
+        Console.ReadLine()
+<<<<<<< HEAD
+        Console.WriteLine(nombres(Index))
+=======
+        'prueba tarea
+
+>>>>>>> desarrollador2
+
+    End Sub
+End Module
